8d7a130d2e4dbb94b18b9ee9d1a39b7d21248d92
Igor <82165182+igor0400@users.noreply.github.com>
2022-04-07 01:46:24 +0300

    Update problems.js

--- src/components/problems/problems.js
+++ src/components/problems/problems.js
@@ -21,7 +21,7 @@ class Problems extends Component {
    componentDidMount() {
       axios
          .get(
-            `https://pharmacy-applications-default-rtdb.firebaseio.com/${this.props.dbLink}.json`
+            `${/*link to firebase*/}/${this.props.dbLink}.json`
          )
          .then((response) => {
             const applications = this.state.applications;
