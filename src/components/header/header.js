bf63a74743d040c02ae0e9f21ef36b03b18899eb
Igor <prostopochtaivso@gmail.com>
2022-04-27 13:28:22 +0300

    arrow back

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,43 @@
+import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import './header.css';
 
 import logo from '../../img/fsimage.jpg';
+import arrow from '../../img/icons/arrow-left.svg';
+
+const paths = [
+  '/:path',
+  '/:path/:city',
+  '/:path/:city/:addres',
+  '/:path/:city/:addres/:id',
+];
 
 const Header = () => {
-   return (
-      <Link to="/" className="a">
-         <div className="header">
-            <img src={logo} alt="logo" className="header-logo" />
-            <h3 className="header-text">Тех. заявки</h3>
-         </div>
+  return (
+    <div className="header">
+      <Route exact path="/">
+        <div style={{ marginLeft: '40px' }}></div>
+      </Route>
+      {paths.map((item) => (
+        <Route exact path={item}>
+          <img
+            src={arrow}
+            alt="arrow"
+            className="arrow-left"
+            onClick={() => window.history.back()}
+          />
+        </Route>
+      ))}
+      <Link to="/">
+        <div className="header-main">
+          <img src={logo} alt="logo" className="header-logo" />
+          <h3 className="header-text">Тех. заявки</h3>
+        </div>
       </Link>
-   );
+      <div></div>
+    </div>
+  );
 };
 
 export default Header;
